a3bb093181a7823583bf6b94365650c2be2f9aa1
Pranavchiku <goswami.4@iitj.ac.in>
2025-06-05 16:18:22 +0530

    XX: use only real32

--- src/splpak.F90
+++ src/splpak.F90
@@ -30,15 +30,15 @@
 
     private
 
-#ifdef REAL32
+! #ifdef REAL32
     integer,parameter :: wp = real32   !! Real working precision [4 bytes]
-#elif REAL64
-    integer,parameter :: wp = real64   !! Real working precision [8 bytes]
-#elif REAL128
-    integer,parameter :: wp = real128  !! Real working precision [16 bytes]
-#else
-    integer,parameter :: wp = real64   !! Real working precision if not specified [8 bytes]
-#endif
+! #elif REAL64
+!     integer,parameter :: wp = real64   !! Real working precision [8 bytes]
+! #elif REAL128
+!     integer,parameter :: wp = real128  !! Real working precision [16 bytes]
+! #else
+!     integer,parameter :: wp = real64   !! Real working precision if not specified [8 bytes]
+! #endif
 
     integer,parameter,public :: splpak_wp = wp   !! Working precision
 
